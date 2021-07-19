882001f8efeaab8392cdb9c5e3873cf96a3297fa
WellingtonJJ <wjc.rio@gmail.com>
2021-07-19 03:03:18 -0300

    Ajuste layout

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
-    <h1>Vejas as noticias:</h1>
-    <h3>Visualizando atualmente {{ viewItems }} de {{totalItems}} notícias</h3>
+    <div class="header">
+      <h1>Hack News</h1>
+      <h3>Visualizando atualmente {{ viewItems }} notícias</h3>
+    </div>
 
-    <input type="button" value="Exibir 10" v-on:click="findNewsIDs(10)"/>
-    <input type="button" value="Exibir 20" v-on:click="findNewsIDs(20)"/>
-    <input type="button" value="Exibir 30" v-on:click="findNewsIDs(30)"/>
 
-    <News
-      v-for="notice in newsIDs"
-      :key="notice"
-      id="news"
-      v-bind:newID="notice"
-    />
+    <input class="btn" type="button" value="Exibir 10" v-on:click="findNewsIDs(10)"/>
+    <input class="btn" type="button" value="Exibir 20" v-on:click="findNewsIDs(20)"/>
+    <input class="btn" type="button" value="Exibir 30" v-on:click="findNewsIDs(30)"/>
+    <div class="newsView">
+      <News
+        v-for="notice in newsIDs"
+        :key="notice"
+        id="news"
+        v-bind:newID="notice"
+      />
+    </div>
   </div>
 </template>
 
@@ -50,7 +54,6 @@ export default {
           });
         })
         .catch((err) => {
-          //TODO tratar erro de requisição
           console.log(err);
         });
     },
@@ -68,15 +71,54 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  /*text-align: center;
+  color: #2c3e50;*/
 }
 
 body {
-  background-color: dodgerblue;
+  background-color: rgb(255, 255, 255);
+  word-wrap: break-word;
+  word-break: break-all;
+  text-align-last: center;
+}
+
+.btn{
+    background-color: #f16d00; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 2px;
+    cursor: pointer;
+}
+
+.btn:hover{
+  background-color: rgb(51, 50, 50);
+  color: white;
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
 }
 
 #news {
-  border: 10px black solid;
+  border: 0px black solid;
 }
+
+.newsView {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+
+
+
 </style>
--- src/components/News.vue
+++ src/components/News.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="main">
-    <h1>
-      <a v-bind:href="link" target="_blank">Título: {{ title }}</a>
-    </h1>
-    <h3>Autor: {{ author }}</h3>
-    <h4>Pontuação: {{ score }}</h4>
+    <div class="principal">
+      <h1>
+        <a v-bind:href="link" target="_blank">Notícia: {{ title }}</a>
+      </h1>
+    </div>
+    <div class="details">
+      <h3>Autor: {{ author }}</h3>
+      <h4>Pontuação: {{ score }}</h4>
+    </div>
+
   </div>
 </template>
 
@@ -37,11 +42,10 @@ export default {
           this.author = res.data.by;
           this.score = res.data.score;
           this.link = res.data.url;
-          // console.log("retorno get",this.link)
         })
         .catch((err) => {
           console.log(err);
-          //TODO tratar erro de requisição
+          
         });
     },
   },
@@ -55,13 +59,39 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .main {
-  background-color: white;
+  align-items: center;
+  background-color: rgb(238, 236, 236);
+  color: rgb(129, 126, 126);
+  padding: 20px;
+  margin: 18px 8px;
+  max-width: 400px;
+  min-width: 400px;
+  border-radius: 30px;
 }
 
 a {
   text-decoration: none;
-  color: black;
+  color: #f16d00;
+  font-size: 25px;
 }
+
+a:hover{
+  color: #e28941;
+  cursor: pointer;
+}
+
+h3, h4{
+  font-size: 15px;
+}
+
+.details{
+  padding-top: 0;
+}
+
+.principal{
+  min-height: 150px;
+}
+
 
 /*
 h3 {
